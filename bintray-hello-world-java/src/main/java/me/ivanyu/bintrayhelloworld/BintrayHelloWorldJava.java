fe2d6fad21f07f6daa31593e6a5613f21e4dd421
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2017-08-06 18:58:29 +0300

    Generate project and add simple class

---
+++ bintray-hello-world-java/src/main/java/me/ivanyu/bintrayhelloworld/BintrayHelloWorldJava.java
@@ -0,0 +1,9 @@
+package me.ivanyu.bintrayhelloworld;
+
+public class BintrayHelloWorldJava
+{
+    public static void say()
+    {
+        System.out.println("[Java] Hello World from Bintray!");
+    }
+}
