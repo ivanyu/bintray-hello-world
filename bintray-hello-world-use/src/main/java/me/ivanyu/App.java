cf333ab669dec180ae21ea43fdbe2cd4ed82c549
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2017-08-06 20:47:04 +0300

    Add using application

---
+++ bintray-hello-world-use/src/main/java/me/ivanyu/App.java
@@ -0,0 +1,15 @@
+package me.ivanyu;
+
+import me.ivanyu.bintrayhelloworld.BintrayHelloWorldJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BintrayHelloWorldJava.say();
+    }
+}
